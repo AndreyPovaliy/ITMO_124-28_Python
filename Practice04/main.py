3ccaf7563f2b1445f2c4b6b8fc74dda6c8884b01
Andrey Povaliy <116582306+AndreyPovaliy@users.noreply.github.com>
2023-04-21 13:26:59 +0300

    Add generator to Python

--- Practice04/main.py
+++ Practice04/main.py
@@ -1,18 +1,32 @@
 from Practice04.calculation import mat_scal_mul, mat_mat_mul
 import time
+import pickle
 
-alpha = 9
-beta = - 13
 
-mat_a = [[2, -5, 5], [1, -4, 9], [5, -1, 3]]
-mat_b = [[2, -1, 7], [1, -6, 3], [6, -9, 7]]
-mat_c = [[4, -3, 9], [5, -4, 1], [3, -5, 3]]
+filename = "datafile.txt"
+
+
+D_2 = []
+with open(filename, "rb") as fh:
+    D_2  = pickle.load(fh)
+
+
+values = list(D_2.values())
+
+alpha = values[0]
+
+beta = values[1]
+
+mat_a = values[2]
+mat_b = values[3]
+mat_c = values[4]
 
 start_time = time.time()
 mat_temp = mat_scal_mul(alpha, mat_a)
 mat_temp1 = mat_mat_mul(mat_temp, mat_b)
 mat_temp2 = mat_scal_mul(beta, mat_c)
 mat_result = mat_temp1 + mat_temp2
+print(mat_result)
 print("--- %s seconds ---" % (time.time() - start_time))
 
-# --- 0.0009968280792236328 seconds ---
+# --- 0.0009958744049072266 seconds ---
--- Practice06/__init__.py
+++ Practice06/__init__.py
@@ -1,3 +1,12 @@
 import zip_util
+import function
+import csv
 
-Command ('loc', 'zip', 'dist', 'end')
+with open("zip_codes_states.csv", encoding='utf-8') as r_file:
+    reader_object = csv.reader('zip_codes_states.csv', delimiter=",")
+    count = 0
+    # Считывание данных из CSV файла
+
+    print(r_file)
+
+
